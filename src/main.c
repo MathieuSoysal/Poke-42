e464536bd3a2e5d3ba5c329776fed404694640cc
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-10-01 12:51:51 +0200

    basic testing of `fgetc`

--- src/main.c
+++ src/main.c
@@ -6,17 +6,25 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 11:55:51 by poss              #+#    #+#             */
-/*   Updated: 2023/10/01 12:22:44 by poss             ###   ########.fr       */
+/*   Updated: 2023/10/01 12:47:30 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "output.h"
+#include "io.h"
 #include "temp.h"
+#include <stdio.h>
 
 int	main(void)
 {
+	int	c;
+
 	if (get42() == 42)
 	{
 		ft_putstr("yo\n");
 	}
+	c = ft_getc_fd(STDIN);
+	if (c == EOF)
+		printf("EOF\n");
+	else
+		printf("the char is %c\n", c);
 }
